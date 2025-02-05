13d379f3ab0e4b686f636fb195e9e7f6d44b48b0
vlad424 <vlad6754687@gmail.com>
2025-02-05 13:18:39 +0700

    main | gets modules

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
-import { PrismaService } from 'src/prisma.servec';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma:PrismaService){}
- 
+  constructor(private prisma: PrismaService) {}
 
-  async getUsers(){
-    
-    return 1
+  async getUsers() {
+    const modules = await this.prisma.module.findMany({
+      include: {
+        chapters: {
+          include: {
+            lessons: true
+          }
+        }
+      }
+    })
+
+    return modules
   }
-
-
 }
