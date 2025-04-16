d619b991c3c5d40cee755a856673462fe7f0cb94
vlad424 <vlad6754687@gmail.com>
2025-04-16 12:06:46 +0700

    main

--- src/training/training.service.ts
+++ src/training/training.service.ts
@@ -8,20 +8,52 @@ export class TrainingService {
   constructor(private prisma: PrismaService) {}
 
   async getAllTrainingModules() {
+    //choose_word inset_word
+    // const module = await this.prisma.module.update({
+    //   where: { id: 2 },
+    //   data: {
+    //     chapters: {
+    //       create: {
+    //         nameChapter: 'хз',
+    //         description: 'aslo xz',
+    //         lessonsCircle: {
+    //           create: {
+    //             lessons: {
+    //               create: {
+    //                 nameLesson: '',
+    //                 description: '',
+    //                 typeId: 1,
+    //                 textContent: 'class component',
+    //               },
+    //             },
+    //           },
+    //         },
+    //       },
+    //     },
+    //   },
+    // });
+
+    // const module = await this.prisma.module.create({
+    //   data: {
+    //     nameModule: "Продвинутый уровеь",
+    //     description: 'Поговорим о структурах'
+    //   }
+    // })
+
     return this.prisma.module.findMany({
       include: {
         chapters: {
-          include:{
-            lessonsCircle:{
-              include:{
-                lessons:{
-                  include:{
-                    exercises: true
-                  }
-                }
-              }
-            }
-          }
+          include: {
+            lessonsCircle: {
+              include: {
+                lessons: {
+                  include: {
+                    exercises: true,
+                  },
+                },
+              },
+            },
+          },
         },
       },
     });
